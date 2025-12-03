7d2cd544bf563f9b34b3f18428a69eb2e7bc77a8
SELFEMO <85822938+SELFEMO@users.noreply.github.com>
2025-12-03 12:43:01 +0800

    fix something...

--- scripts/data_clean.py
+++ scripts/data_clean.py
@@ -56,18 +56,23 @@ def inspect_data(
 
 def clean_data(
         data_path: Optional[str] = None,
+        save_path: Optional[str] = '../data/dataset',
 ):
     """
     Cleans the dataset at the given path by removing corrupted images.  通过删除损坏的图像来清理给定路径下的数据集。
     At the same time, not copy images starting with 'mirrored_'.  同时，不复制以 'mirrored_' 开头的图像。
 
     :param data_path: The local path where the dataset is stored. If None, defaults to '../data/Wound_dataset copy'.  本地存储数据集的路径。如果为 None，则默认为 '../data/Wound_dataset copy'。
+    :param save_path: The local path where the cleaned dataset should be saved. If None, defaults to '../data/dataset'.  清理后的数据集应保存的本地路径。如果为 None，则默认为 '../data/dataset'。
     """
     if data_path is None or data_path.strip() == "":  # Default data path  # 默认数据路径
         data_path = os.path.join('../data/Wound_dataset copy')
-    if os.path.exists('../data/dataset'):  # If cleaned dataset directory exists  # 如果清理后的数据集目录存在
-        shutil.rmtree('../data/dataset')  # Remove existing cleaned dataset directory  # 删除现有的清理后数据集目录
-    os.makedirs('../data/dataset')  # Create cleaned dataset directory  # 创建清理后的数据集目录
+    if save_path is None or save_path.strip() == "":  # Default save path  # 默认保存路径
+        save_path = os.path.join('../data/dataset')
+    save_path = save_path.strip()  # Remove leading/trailing whitespace  # 删除前导/尾随空格
+    if os.path.exists(save_path):  # If cleaned dataset directory exists  # 如果清理后的数据集目录存在
+        shutil.rmtree(save_path)  # Remove existing cleaned dataset directory  # 删除现有的清理后数据集目录
+    os.makedirs(save_path)  # Create cleaned dataset directory  # 创建清理后的数据集目录
     corrupted_images = []  # List to hold paths of corrupted images  # 用于保存损坏图像路径的列表
     index = 1  # Index for renaming files  # 用于重命名文件的索引
     for label in os.listdir(data_path):
@@ -106,4 +111,7 @@ if __name__ == "__main__":
 
     inspect_data(data_path)
 
-    clean_data(data_path)
+    clean_data(
+        data_path,
+        save_path='../data/dataset',
+    )
