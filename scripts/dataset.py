a82b3d697fd0eceaa6e53f72ec2ff4952791f7f0
SELFEMO <85822938+SELFEMO@users.noreply.github.com>
2025-12-03 12:25:08 +0800

    fix something...

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -236,8 +236,8 @@ class data_loader:
         if self.use_augmentation and not self.is_last:
             image = apply_augmentation(image)
 
-        # image = numpy.array(image) / 255.0  # Normalize pixel values to [0, 1]  将像素值归一化到 [0, 1]
-        image = numpy.array(image) / 255.0 - 0.5  # Normalize pixel values to [-0.5, 0.5]  将像素值归一化到 [-0.5, 0.5]
+        image = numpy.array(image) / 255.0  # Normalize pixel values to [0, 1]  将像素值归一化到 [0, 1]
+        # image = numpy.array(image) / 255.0 - 0.5  # Normalize pixel values to [-0.5, 0.5]  将像素值归一化到 [-0.5, 0.5]  # BUG: this normalization seems to cause issues
 
         return image, data['label_index'], data['index']
 
